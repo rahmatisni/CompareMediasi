2a897b5dd036f9e17af823a91646d5c3b31c71ee
Rahmat Nur Isni <rahmatisni@gmail.com>
2025-01-25 21:49:29 +0700

    add timediff

--- exe.py
+++ exe.py
@@ -389,7 +389,7 @@ try:
                             tarif,
                             sisa_saldo,
                             created_at,
-                            TIMESTAMPDIFF(MINUTE, created_at, tgl_transaksi) AS selisih_menit
+                            TIMESTAMPDIFF(MINUTE, create_at, tgl_transaksi) AS selisih_menit
                             FROM {db_name}.jid_transaksi_deteksi
                             WHERE etoll_id IN ({card_number_str})
                             AND tarif != 0 
